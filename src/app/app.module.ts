ef2e7a695c7ce22e42a554a587897e011ccbf5b7
StevenEscobarC <stevenlikeplis@gmail.com>
2022-03-18 22:38:56 -0500

    event binding, html, css y typescript. Ejercicio suma con componente reactivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './saludo/saludo.component';
+import { PadreComponent } from './padre/padre.component';
+import { Hijo1Component } from './hijo1/hijo1.component';
+import { Hijo2Component } from './hijo2/hijo2.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { SumarComponent } from './sumar/sumar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludoComponent
+    SaludoComponent,
+    PadreComponent,
+    Hijo1Component,
+    Hijo2Component,
+    EventosComponent,
+    SumarComponent
   ],
   imports: [
     BrowserModule
--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaludoComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string;
+  identifParrafo: string;
+  identifInput: string;
+
+  constructor() { 
+    this.mensaje = 'Un saludo a todos';
+    this.identifParrafo = 'parrafoPrincipal';
+    this.identifInput = 'text';
+
+    //función anónima
+    setTimeout(() => {  
+      this.mensaje = 'Otro mensaje diferente';
+      this.identifParrafo = 'main';
+      this.identifInput = 'password';
+    }, 3000)
+  }
 
   ngOnInit(): void {
+  }
+
+  mostrarSaludo(): string{
+    return 'Saludo desde el método';
   }
 
 }
